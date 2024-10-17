a046bca3fd53d89f3aba5b4728e5340e7ce35504
AswathyJL <aswathyjlal@gmail.com>
2024-10-17 12:31:10 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ee0181',
+        'secondary' : '#e2f3d0a1'
+
+      },
+      fontFamily: {
+        'primary':['Roboto','sans-serif']
+      },
+      animation: {
+        'wiggle': 'wiggle 2s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        },
+      }, 
+    },
+  },
+  plugins: [],
+}
+
